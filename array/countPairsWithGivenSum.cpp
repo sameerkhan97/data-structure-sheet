8f0817f247b9852b84792c00ad4ed7d3d19165ea
sameer khan <74293949+sameerkhan97@users.noreply.github.com>
2021-03-03 15:49:54 +0530

    add code description and rename

--- array/countPairsWithGivenSum.cpp
+++ array/countPairsWithGivenSum.cpp
@@ -1,4 +1,4 @@
-/*Given an array of N integers, and an integer K, find the number of pairs of elements in the array whose sum is equal to K.
+//Given an array of N integers, and an integer K, find the number of pairs of elements in the array whose sum is equal to K.
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -6,35 +6,30 @@ using namespace std;
 class Solution{   
 public:
     int getPairsCount(int arr[], int n, int k) {
+        //c-count number of pairs
         int c=0;
         for(int i=0;i<n;i++)
         {
+            //pairing a[i] with all forward element to check if sum equals k
             for(int j=i+1;j<n;j++)
             {
                 if(arr[i]+arr[j]==k)
-                    c++;
+                    c++;    //when sum equals k count the pair
             }
         }
         return c;
     }
 };
-
-// { Driver Code Starts.
-
 int main() {
-    int t;
-    cin >> t;
-    while (t--) {
+    //n-array size,k-sum to be checked
         int n, k;
         cin >> n >> k;
         int arr[n];
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++){
             cin >> arr[i];
-        }
+        
         Solution ob;
         auto ans = ob.getPairsCount(arr, n, k);
-        cout << ans << "\n";
-    }
-    
-    return 0;
-}  // } Driver Code Ends
+        cout << ans << "\n";  
+        return 0;
+}  
