de26fe7efe41ed68542d2aef75c6eab9e9f3e9d0
sameer khan <74293949+sameerkhan97@users.noreply.github.com>
2021-03-03 17:38:33 +0530

    add code description and rename

--- string/reverseString.cpp
+++ string/reverseString.cpp
@@ -3,7 +3,9 @@ using namespace std;
 int main() 
 {
     string str="Hello"; 
+    //take 2 pointers pointing 1st and last character of String
     int start=0,end=str.length()-1;
+    //swap element at start & end untill start exceeds end
     while(start<end){
         char t=str[start];
         str[start]=str[end];
