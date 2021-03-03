1f612895923b6d7efdbab020b4d01064402a9e55
sameer khan <74293949+sameerkhan97@users.noreply.github.com>
2021-03-03 16:29:48 +0530

    add code description and rename

--- array/minimumNumberOfJumps.cpp
+++ array/minimumNumberOfJumps.cpp
@@ -1,4 +1,4 @@
-/* Q-Given an array of integers where each element represents the max number of steps that can be made forward from that element. 
+/* Q-Given an array of  integers where each element represents the max number of steps that can be made forward from that element. 
 Find the minimum number of jumps to reach the end of the array (starting from the first element). If an element is 0, then you cannot move through that element. */
 #include<bits/stdc++.h>
 using namespace std;
