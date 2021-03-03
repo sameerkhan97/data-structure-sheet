80c880f11d3a9db3f46c662ce8502241ca667b01
sameer khan <74293949+sameerkhan97@users.noreply.github.com>
2021-03-03 16:29:25 +0530

    Update and rename ARRAY/9Minimum number of jumps.cpp to array/minimumNumberOfJumps.cpp

--- array/minimumNumberOfJumps.cpp
+++ array/minimumNumberOfJumps.cpp
@@ -1,14 +1,12 @@
-// Q-Given an array of integers where each element represents the max number of steps that can be made forward from that element. Find the minimum number of jumps to reach the end of the array (starting from the first element). If an element is 0, then you cannot move through that element.
+/* Q-Given an array of integers where each element represents the max number of steps that can be made forward from that element. 
+Find the minimum number of jumps to reach the end of the array (starting from the first element). If an element is 0, then you cannot move through that element. */
 #include<bits/stdc++.h>
 using namespace std;
-
-
- // } Driver Code Ends
-
-
 // Function to return minimum number of jumps to end of array
 int minJumps(int arr[], int n){
+    //c-count no of jumps
     int i=0,c=0;
+  //i will jump untill last element and c will be counting no. of jumps
     while(i<n-1)
     {
         i=i+arr[i];
@@ -16,16 +14,8 @@ int minJumps(int arr[], int n){
     }
     return c;
 }
-
-
-// { Driver Code Starts.
-
 int main()
 {
-    int t;
-    cin>>t;
-    while(t--)
-    {
         int n,i,j;
         cin>>n;
         int arr[n];
@@ -33,7 +23,5 @@ int main()
             cin>>arr[i];
             
         cout<<minJumps(arr, n)<<endl;
-    }
     return 0;
 }
-  // } Driver Code Ends
