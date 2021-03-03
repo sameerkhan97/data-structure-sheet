49c0675348fe83c10cc67f1882d6663d53bdacb1
sameer khan <74293949+sameerkhan97@users.noreply.github.com>
2021-03-03 16:24:53 +0530

    add code description and rename

--- ARRAY/6Cyclically rotate an array by one.cpp
+++ ARRAY/6Cyclically rotate an array by one.cpp
@@ -2,13 +2,8 @@
 
 #include <stdio.h>
 void rotate(int arr[], int n);
-
 int main()
 {
-    int t;
-    scanf("%d",&t);
-    while(t--)
-    {
         int n;
         scanf("%d",&n);
         int a[n] , i;
@@ -18,22 +13,21 @@ int main()
         for (i = 0; i < n; i++)
             printf("%d ", a[i]);
         printf("\n");
-    }
         return 0;
 }
-// } Driver Code Ends
-
-
 void rotate(int arr[], int n)
 {
+    //storing last element of array in temp
     int temp=arr[n-1],i=0;
     int t,extra=arr[0];
     while(i<n-1)
     {
+        //copying each element in its next index untill last element gets copied 
         t=arr[i+1];
         arr[i+1]=extra;
         extra=t;
         i++;
     }
+    //storing last element in 1st place
     arr[0]=temp;
 }
