42949509826ad0d4e5403a6f64c77d51f20b5436
sameer khan <74293949+sameerkhan97@users.noreply.github.com>
2021-03-03 17:34:50 +0530

    add code description and rename

--- matrix/majorityElement(Nby3Times).cpp
+++ matrix/majorityElement(Nby3Times).cpp
@@ -6,9 +6,13 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         vector<int> s;
+        //when we talk about n/3 times there may be 2 elements that appear more than n/3 time
+        //hence taking el1,el2 and c1,c2 to count occurance
         int el1=-1,el2=-1,c1=0,c2=0;
         for(int num : nums)
         {
+            //checking in xcurrent num equals el1/el2 then increasing c1/c2
+            //if c1/c2 equals zerop then assign new num to el1/el2 and count its occurance
             if(num==el1)
                 c1++;
             else if(num==el2)
@@ -29,8 +33,10 @@ public:
                 c2--;
             }
         }
+        //after getting 2 elements el1,el2 count only thse 2 elements occurance in matrix again
         c1=0;
         c2=0;
+        
         for(int num : nums)
         {
             if(num==el1)
@@ -38,6 +44,7 @@ public:
             else if(num==el2)
                 c2++;
         }
+        //if(el1/el2 occurs more than n/3 then insert it in array
         if(c1>nums.size()/3)
             s.push_back(el1);
         if(c2>nums.size()/3)
