572e201179b3b9fd95be94d05e7af6dddb07641d
sameer khan <74293949+sameerkhan97@users.noreply.github.com>
2021-03-03 15:59:57 +0530

    add code description and rename

--- array/sortArrayOf0s,1s,2s.cpp
+++ array/sortArrayOf0s,1s,2s.cpp
@@ -4,14 +4,17 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int l=0,mid=0,h=nums.size()-1;
-        
+       //iterate mid in the array and perform actions accordingle to mid element
         while(mid<=h)
         {
             switch(nums[mid])
             {
+                //if mid is at 0 then swap it with l 
                 case 0:swap(nums[l],nums[mid]);
                         mid++;l++;break;
+                //if//mid is at 1 just increase mid
                 case 1:mid++;break;
+                //when mid is 2 swap it with h and decrease h only
                 case 2:swap(nums[mid],nums[h]);
                         h--;break;
             }
