8fd576ab4fc9af8181d5d08b4cb430dd84dbd8e8
sameer khan <74293949+sameerkhan97@users.noreply.github.com>
2021-03-03 16:19:40 +0530

    add code description and rename

--- array/moveAllTheNegativeElementsToOneSideOfArray.cpp
+++ array/moveAllTheNegativeElementsToOneSideOfArray.cpp
@@ -5,11 +5,13 @@ using namespace std;
 
 int main() {
 	int a[]={1,5,4,-9,-1,-21,-58,1},i,j;
-	int n=sizeof(a)/sizeof(a[i]);
+	int n=sizeof(a)/sizeof(a[0]);
+	//iterating array with i
 	for(i=0;i<n;i++)
 	{
 		if(a[i]>0)
 		{
+			//if a[i] is positive & any element after a[i] is negative swap it with a[i]
 			for(j=i+1;j<n;j++)
 			{
 				if(a[j]<0)
