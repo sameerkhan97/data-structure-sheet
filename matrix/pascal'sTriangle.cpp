c9eb51c3a7cc6d38a99ee17f9f8ee770880beda6
sameer khan <74293949+sameerkhan97@users.noreply.github.com>
2021-03-03 17:14:45 +0530

    add code description and rename

--- matrix/pascal'sTriangle.cpp
+++ matrix/pascal'sTriangle.cpp
@@ -10,12 +10,13 @@ public:
        vector<vector<int>> p(numRows);
         for(int i=0;i<numRows;i++)
         {
+            //resize every row according to row number bcoz in pascal series size of row(i)=i+1
             p[i].resize(i+1);
+            //mark 1st and last element of each row as one
             p[i][0]=p[i][i]=1;
             for(int j=1;j<i;j++)
-            {
+                //in pascals series every element is sum of its above 2 elements
                 p[i][j]=p[i-1][j-1]+p[i-1][j];
-            }
         }
         return p;
     }
