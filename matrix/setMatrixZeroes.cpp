8e705d1bf2131dfa882fa474fb6fc198686e2f8c
sameer khan <74293949+sameerkhan97@users.noreply.github.com>
2021-03-03 17:06:27 +0530

    Update and rename Matrix/1Set Matrix Zeroes.cpp to matrix/setMatrixZeroes.cpp

--- matrix/setMatrixZeroes.cpp
+++ matrix/setMatrixZeroes.cpp
@@ -9,6 +9,7 @@ public:
             {
                 if(matrix[m][n]==0)
                 {
+		   //when 0 is found then mark all its column and row with a unique key(i choose -786)
                     for(int i=0;i<matrix[0].size();i++)
                     {
                         if(matrix[m][i]!=0)
@@ -22,6 +23,7 @@ public:
                 }
             }
         }
+	//iterate in whole matrix and whenever find unique key replace it with 0
         for(int m=0;m<matrix.size();m++)
         {
             for(int n=0;n<matrix[0].size();n++)
@@ -44,6 +46,7 @@ public:
         {
             for(int n=0;n<matrix[0].size();n++)
             {
+		//when found 0 then mark its column and row index as 0 in row,col array
                 if(matrix[m][n]==0)
                 {
                     row[m]=0;
@@ -51,6 +54,7 @@ public:
                 }
             }
         }
+	//iterate whole matrix and for any element if row/col array has 0 index then mark it as 0
         for(int m=0;m<matrix.size();m++)
         {
             for(int n=0;n<matrix[0].size();n++)
@@ -68,13 +72,16 @@ public:
         int col0=1;
         for(int m=0;m<matrix.size();m++)
         {
+	   //instead of using seprate space ,use 1st row and columns as array
             if(matrix[m][0]==0) col0=0;
             for(int n=1;n<matrix[0].size();n++)
             {
+		//whenever 0 found (except in 1st row/col) then marked 1 row's corresponding index and 1st column's corresponding index as 0 
                 if(matrix[m][n]==0)
                     matrix[m][0]=matrix[0][n]=0;
             }
         }
+	//iterate whole matrix and for every element check its index in 1st row and column if anyone is marked 0 then mark element as 0
         for(int m=matrix.size()-1;m>=0;m--)
         {
             for(int n=matrix[0].size()-1;n>=0;n--)
