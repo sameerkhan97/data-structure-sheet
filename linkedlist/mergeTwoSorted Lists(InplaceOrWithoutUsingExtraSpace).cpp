e6d891c050efa1ccd94264392844fcfbc1c2fd4d
sameer khan <74293949+sameerkhan97@users.noreply.github.com>
2021-03-05 14:43:43 +0530

    fix type mistake

--- linkedlist/mergeTwoSorted Lists(InplaceOrWithoutUsingExtraSpace).cpp
+++ linkedlist/mergeTwoSorted Lists(InplaceOrWithoutUsingExtraSpace).cpp
@@ -1,6 +1,5 @@
 //Merge two sorted linked lists and return it as a sorted list. The list should be made by splicing together the nodes of the first two lists.
-//CPP CODE :
- /* Definition for singly-linked list.
+/* Definition for singly-linked list.
    struct ListNode {
       int val;
       ListNode *next;
@@ -36,49 +35,3 @@ public:
         return res;
     }
 };
-
-
-
-//JAVA Code :
-/* Definition for singly-linked list.
-   public class ListNode {
-      int val;
-      ListNode next;
-      ListNode() {}
-      ListNode(int val) { this.val = val; }
-      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-  } */
-
-class Solution {
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if(l1==null)    return l2;
-        if(l2==null)    return l1;
-        //l1 will point to the smaller node 
-        if(l1.val>l2.val)
-        {
-            ListNode t=l1;
-            l1=l2;
-            l2=t;
-        }
-        //res-will keep pointer to first Node
-        ListNode res=l1;
-        while(l1!=null && l2!=null)
-        {
-            ListNode temp=null;
-            //moving l1 in LL untill it points to >value that l2
-            while(l1!=null && l1.val<=l2.val)
-            {
-                //temp will be pointing one previous node of l1
-                temp=l1;
-                l1=l1.next;             
-            }
-            //one previous node of l1 will now popint to node at l2
-            temp.next=l2;
-            //swap l1,l2 because l1 will always point to smaller Node
-            ListNode t=l1;
-            l1=l2;
-            l2=t;
-        }
-        return res;
-    }
-}
