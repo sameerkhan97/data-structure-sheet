b067007958920900e74757396e65fcf6a8649815
sameer khan <74293949+sameerkhan97@users.noreply.github.com>
2021-03-12 23:07:39 +0530

    fix type mistake

--- linkedlist/removeNthNodeFromEndOfList(Optimal).cpp
+++ linkedlist/removeNthNodeFromEndOfList(Optimal).cpp
@@ -1,4 +1,4 @@
-//Given the head of a linked list, remove the nth node from the end of the list and return its head.
+//Given the head of a linked list,  remove the nth node from the end of the list and return its head.
 //Optimal Approach :
 /* Definition for singly-linked list.
   struct ListNode {
