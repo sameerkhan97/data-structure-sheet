f8247095a756a246e84d69f5a61d1d35fcd2ad94
sameer khan <74293949+sameerkhan97@users.noreply.github.com>
2021-05-01 20:56:03 +0530

    add new problems

--- searching sorting/majorityElement.cpp
+++ searching sorting/majorityElement.cpp
@@ -6,7 +6,7 @@ Input: N = 3     A[] = {1,2,3}
 Output: -1
 Explanation: Since, each element in {1,2,3} appears only once so there is no majority element.
 Example 2:
-Input: N = 5   A[] = {3,1,3,3,2} 
+Input: N = 5   A[] = {3,1,3,3,2}  
 Output: 3
 Explanation: Since, 3 is present more than N/2 times, so it is the majority element.   */
 
