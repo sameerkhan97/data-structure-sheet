32dd6df922eaf8ec5aafdf21d6f79836b9856139
sameer khan <74293949+sameerkhan97@users.noreply.github.com>
2021-05-01 20:58:13 +0530

    add new problems

--- searching sorting/smallestFactorialNumber.cpp
+++ searching sorting/smallestFactorialNumber.cpp
@@ -3,7 +3,7 @@ Example 1:
 Input: n = 1 , Output: 5
 Explanation : 5! = 120 which has at least 1 trailing 0.
 
-Example 2:
+Example 2: 
 Input: n = 6 , Output: 25
 Explanation : 25! has at least 6 trailing 0.           */
 
