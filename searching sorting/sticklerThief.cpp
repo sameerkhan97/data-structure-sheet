cce90d0c2b48856137896e0807b77d0e361fd8f8
sameer khan <74293949+sameerkhan97@users.noreply.github.com>
2021-05-01 20:58:56 +0530

    add new problems

--- searching sorting/sticklerThief.cpp
+++ searching sorting/sticklerThief.cpp
@@ -2,7 +2,7 @@
   when looting the houses. According to the rule, he will never loot two consecutive houses. At the same time, he wants to maximize the amount
   he loots. The thief knows which house has what amount of money but is unable to come up with an optimal looting strategy. He asks for your
   help to find the maximum money he can get if he strictly follows the rule. Each house has a[i] amount of money present in it.
-
+ 
 Example 1:
 Input: n = 6    a[] = {5,5,10,100,10,5}
 Output: 110
