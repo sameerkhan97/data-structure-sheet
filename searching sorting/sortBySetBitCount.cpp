e21573786e018b2782690b99682207123e150cae
sameer khan <74293949+sameerkhan97@users.noreply.github.com>
2021-05-01 20:58:33 +0530

    add new problems

--- searching sorting/sortBySetBitCount.cpp
+++ searching sorting/sortBySetBitCount.cpp
@@ -1,7 +1,7 @@
 /*Given an array of integers, sort the array (in descending order) according to count of set bits in binary representation of array elements. 
 Note: For integers having same number of set bits in their binary representation, sort according to their position in the original array 
       i.e., a stable sort.
-Example 1:
+Example 1: 
 Input: arr[] = {5, 2, 3, 9, 4, 6, 7, 15, 32};
 Output: 15 7 5 3 9 6 2 4 32
 Explanation: The integers in their binary representation are:
